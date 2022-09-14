7ed4abe1cb0c017b02f7be3809b7c60508306f7e
Pathik6299 <113482596+Pathik6299@users.noreply.github.com>
2022-09-13 20:00:37 -0400

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+print("Hello", name + "!")
+a = int(input("Enter your first value:"))
+b = int(input("Enter your second value:"))
+res=a+b
+print(res)
+res=a-b
+print(res)
+res=a*b
+print(res)
+res=a**b
+print(res)
+res=a/b
+print(res)
